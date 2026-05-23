53ef986afcc80739c345a506b82ed0a043fcbd11
martinristow <martinristov084@gmail.com>
2026-05-23 18:07:33 +0200

    02 to 03

--- app/components/SkillsSection/SkillsSection.tsx
+++ app/components/SkillsSection/SkillsSection.tsx
@@ -13,7 +13,7 @@ const SkillsSection = () => {
                 {/* HEADING */}
                 <div className="flex flex-col gap-5 lg:gap-8">
                     <div className="flex items-center gap-2">
-                        <h5 className="uppercase tracking-widest text-xl leading-none">02</h5>
+                        <h5 className="uppercase tracking-widest text-xl leading-none">03</h5>
                         <div className="flex items-center gap-2">
                             <span className="block w-6 md:w-10 h-0.5 bg-accent self-center"></span>
                             <h3 className="uppercase tracking-widest leading-none">Skills</h3>
